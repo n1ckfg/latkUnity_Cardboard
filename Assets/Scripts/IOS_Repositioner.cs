3559de8490e4041e4f693cd9c1de9ac4af56052a
Nick Fox-Gieg <nick@fox-gieg.com>
2018-06-22 22:37:24 -0400

    first

---
+++ Assets/Scripts/IOS_Repositioner.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IOS_Repositioner : MonoBehaviour {
+
+	public float xShift = 0.5f;
+
+	void Start() {
+		#if UNITY_IOS
+		transform.Translate(xShift, 0f, 0f);
+		#endif
+	}
+
+}
